f2242339c2c9ba726228e8ba3a6c226f23ceb092
jmarcosdias <jmsdias6@gmail.com>
2022-05-14 00:02:12 +0000

    Define the initial model
    User imported from django auth
    Recipe with: title, short description, ingredients, method and a
    foreign key to User

--- whats_for_dinner_app/models.py
+++ whats_for_dinner_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Recipe(models.Model):
+    title = models.CharField(max_length=150, unique=True)
+    slug = models.SlugField(max_length=150, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="tasty_recipe"
+    )
+    short_description = models.TextField(blank=True)
+    ingredients = models.TextField(blank=True)
+    method = models.TextField(blank=True)
+
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self):
+        return self.title
