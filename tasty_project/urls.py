c63dff7e5eb4a6b5dcde4720ef6f6a00608a152d
jmarcosdias <jmsdias6@gmail.com>
2022-05-15 13:37:24 +0000

    Prepare view and config url for recipe list

--- tasty_project/urls.py
+++ tasty_project/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
+    path('', include('whats_for_dinner_app.urls'), name='whats_for_dinner_app_urls'),
 ]
--- whats_for_dinner_app/admin.py
+++ whats_for_dinner_app/admin.py
@@ -4,7 +4,7 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Recipe)
 class RecipeAdmin(SummernoteModelAdmin):
-    list_display = ('title', 'slug','author')
+    list_display = ('title', 'slug', 'author')
     search_fields = ['title', 'short_description', 'ingredients', 'method']
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('short_description', 'ingredients', 'method',)
