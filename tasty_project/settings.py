29489f85c8173a7aa33c8ea2e34fdeaad8dfcad8
jmarcosdias <jmsdias6@gmail.com>
2022-05-18 23:21:32 +0000

    Implement authorization
    Using django-allauth, implement the create account and the log in
    user stories.

--- tasty_project/settings.py
+++ tasty_project/settings.py
@@ -49,12 +49,23 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
     'django_summernote',
     'whats_for_dinner_app',
 ]
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- tasty_project/urls.py
+++ tasty_project/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include('whats_for_dinner_app.urls'), name='whats_for_dinner_app_urls'),
+    path('accounts/', include('allauth.urls')),
 ]
