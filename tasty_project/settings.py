1ab1058a1e67014f0ee1c08d95ffa212b25eb945
jmarcosdias <jmsdias6@gmail.com>
2022-05-28 08:33:19 +0000

    Resolve issue when setting debug to false
    Copy static directory to staticfiles.
    Remove STATICFILES_STORAGE line from settings.py.

--- tasty_project/settings.py
+++ tasty_project/settings.py
@@ -33,7 +33,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Defines if it is testing
 TEST = 'test' in sys.argv
@@ -166,7 +166,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+# STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
